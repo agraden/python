59f37b18602a78f06fb150f727ce73e4619ac6b5
Alexander Graden <97418444+agraden@users.noreply.github.com>
2022-04-29 10:17:13 -0400

    Add files via upload

---
+++ who_am_i.py
@@ -0,0 +1,24 @@
+name = 'Alexander Graden'
+age = 21 
+height = 67 # inches
+height_in_centimeters = height * 2.54
+weight = 145 # lbs
+weight_in_kilograms = weight / 2.205
+eyes = 'blue'
+teeth = 'white'
+hair = 'blonde'
+addictions = 'coffee'
+
+print(f"Let's talk about {name}.")
+print(f"He is {height} inches tall.")
+print(f"That is also {height_in_centimeters} centimeters tall.")
+print(f"He is {weight} pounds heavy.")
+print(f"That is also {weight_in_kilograms} kilograms heavy.")
+print("Is that heavy?")
+print(f"He has {eyes} eyes and {hair} hair.")
+print(f"Their teeth are {teeth}")
+print(f"He loves {addictions}, and can't live without it.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
